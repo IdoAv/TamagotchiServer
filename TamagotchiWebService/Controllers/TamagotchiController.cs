ab1504812f513fedbbab0a29b53224e7e3f0ddf8
idoaviv19031@hotmail.com <idoaviv19031@hotmail.com>
2021-01-24 14:18:15 +0200

    login/logout

--- TamagotchiWebService/Controllers/TamagotchiController.cs
+++ TamagotchiWebService/Controllers/TamagotchiController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TamagotchiBL.Models;
+using TamagotchiWebService.DataTransferObjects;
 
 namespace TamagotchiWebService.Controllers
 {
@@ -18,11 +19,39 @@ namespace TamagotchiWebService.Controllers
             this.context = context;
         }
 
-        [Route("Test")]
-        [HttpGet]
-        public string Test()
+        [Route("Login")]
+        [HttpPost]
+        public PlayerDTO Login([FromBody] UserDTO user)
         {
-            return "it works";
+            Player player = context.LogIn(user.Email, user.Password);
+            if(player != null)
+            {
+                PlayerDTO playerDTO = new PlayerDTO(player);
+                HttpContext.Session.SetObject("loggedIn", playerDTO);
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                return playerDTO;
+            }
+            else
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return null;
+
+            }
+        }
+        [Route("Logout")]
+        [HttpGet]
+        public void Logout()
+        {
+            PlayerDTO player = HttpContext.Session.GetObject<PlayerDTO>("loggedIn");
+            if (player != null)
+            {
+                HttpContext.Session.Clear();
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            }
+            else
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+            }
         }
     }
 }
---
+++ TamagotchiWebService/DataTransferObjects/PlayerDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TamagotchiBL.Models;
+
+namespace TamagotchiWebService.DataTransferObjects
+{
+    public class PlayerDTO
+    {
+        public int PlayerId { get; set; }
+        public string PlayerEmail { get; set; }
+        public string PlayerUserName { get; set; }
+        public string PlayerGender { get; set; }
+        public DateTime? PlayerBirthDate { get; set; }
+        public string PlayerFirstName { get; set; }
+        public string PlayerLastName { get; set; }
+        public int? PlayerActiveAnimal { get; set; }
+        public string PlayerPassword { get; set; }
+
+        public PlayerDTO() { }
+        public PlayerDTO(Player player)
+        {
+            this.PlayerId = player.PlayerId;
+            this.PlayerEmail = player.PlayerEmail;
+            this.PlayerUserName = player.PlayerUserName;
+            this.PlayerGender = player.PlayerGender;
+            this.PlayerBirthDate = player.PlayerBirthDate;
+            this.PlayerFirstName = player.PlayerFirstName;
+            this.PlayerActiveAnimal = player.PlayerActiveAnimal;
+            this.PlayerLastName = player.PlayerFirstName;
+            this.PlayerPassword = player.PlayerPassword;
+        }
+    }
+}
