eb90fa40a942b43555dba675eebd09cc3322611d
IdoAv <60872717+IdoAv@users.noreply.github.com>
2021-01-24 12:12:38 +0200

    Update Program.cs

--- TamagotchiWebService/Program.cs
+++ TamagotchiWebService/Program.cs
@@ -14,7 +14,7 @@ namespace TamagotchiWebService
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-           //h  
+           //hii  
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
