8def7808e8e494bdfb7778bbfcfc8e058dcacce3
liron <liron.abovich@gmail.com>
2021-01-27 11:28:04 +0200

    Commit all

--- TamagotchiWebService/Controllers/TamagotchiController.cs
+++ TamagotchiWebService/Controllers/TamagotchiController.cs
@@ -63,5 +63,58 @@ namespace TamagotchiWebService.Controllers
             context.Register(p);
             Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
         }
+        [Route("Login")]
+        [HttpPost]
+        public PlayerDTO Login([FromBody] UserDTO user)
+        {
+            Player player = context.LogIn(user.Email, user.Password);
+            if (player != null)
+            {
+                PlayerDTO playerDTO = new PlayerDTO(player);
+                HttpContext.Session.SetObject("loggedIn", playerDTO);
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                return playerDTO;
+            }
+            else
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+                return null;
+
+            }
+        }
+        [Route("Logout")]
+        [HttpGet]
+        public void Logout()
+        {
+            PlayerDTO player = HttpContext.Session.GetObject<PlayerDTO>("loggedIn");
+            if (player != null)
+            {
+                context.SaveChanges();
+                HttpContext.Session.Clear();
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            }
+            else
+            {
+                Response.StatusCode = (int)System.Net.HttpStatusCode.Forbidden;
+            }
+        }
+        [Route("GetAnimals")]
+        [HttpGet]
+        public List<AnimalDTO> GetAnimals()
+        {
+            PlayerDTO player = HttpContext.Session.GetObject<PlayerDTO>("loggedIn");
+            if (player != null)
+            {
+                List<AnimalDTO> animals = new List<AnimalDTO>();
+                foreach (Animal a in context.LogIn(player.PlayerEmail, player.PlayerPassword).GetAnimals())
+                {
+                    animals.Add(new AnimalDTO(a));
+                }
+                Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+                return animals;
+            }
+            Response.StatusCode = (int)System.Net.HttpStatusCode.OK;
+            return null;
+        }
     }
 }
